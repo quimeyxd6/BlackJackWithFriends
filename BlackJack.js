59986fc003cf6e8ef68203cdbc62dc4c1bde32d0
qquimmey <quimeyxd6@gmail.com>
2025-05-06 22:52:39 -0300

    Making changes

--- BlackJack.js
+++ BlackJack.js
@@ -22,14 +22,18 @@ function mezclarBaraja(baraja) {
  }
   //Suffle de la baraja  
 
-function repartirCarta(baraja, jugadorId) {
-    const carta = baraja.pop();
-    const divJugador = document.getElementById(jugadorId);
-    const cartaEl = document.createElement('span');
+  function crearCarta(valor, palo, idMano) {
+    const divMano = document.getElementById(idMano);
+    const cartaEl = document.createElement('div');
     cartaEl.className = 'carta';
-    cartaEl.innerText = `${carta.valor}${carta.palo}`;
-    divJugador.appendChild(cartaEl);
-    return carta;
+  
+    cartaEl.innerHTML = `
+      <div class="valor-superior">${valor}</div>
+      <div class="palo">${palo}</div>
+      <div class="valor-inferior">${valor}</div>
+    `;
+  
+    divMano.appendChild(cartaEl);
   }
   //Reparte una carta a un jugador y la muestra en el div correspondiente
 
@@ -44,12 +48,14 @@ function repartirCarta(baraja, jugadorId) {
   // Repartir 2 cartas a cada jugador
   for (let i = 0; i < 2; i++) {
     for (let j = 0; j <= 6; j++) {
-      const carta = repartirCarta(baraja, `p${j}`);
+      const carta = crearCarta(baraja, `p${j}`);
       manos[`p${j}`].push(carta);
     }
-    const cartaDealer = repartirCarta(baraja, 'dealer');
+    const cartaDealer = crearCarta(baraja, 'dealer');
     manos.dealer.push(cartaDealer);
   }
   
  //Por ahora el codigo le da dos cartas al azar a los jugadores pero no al dealer
  //Lo sigueinte es permitir que el dealer saque cartas y que los jugadores puedan pedir cartas o plantarse
+
+ console.log(manos)
